711109e004a417ee4f8aff2252feceacd45476c5
bitPimps <bitpimps@gmail.com>
2020-05-22 16:00:43 -0500

    Gallery update
    Update work page with gallery, need to optimize images

--- wwwRoot/work/index.php
+++ wwwRoot/work/index.php
@@ -6,6 +6,31 @@
 		<title>Work</title>
 		<meta name="description" content="">
 		<?php include_once("../inc/meta-data.php"); ?>
+		<style>
+#gallery img {
+	 height: 75vw;
+	 object-fit: cover;
+}
+ @media (min-width: 576px) {
+	 #gallery img {
+		 height: 35vw;
+	}
+}
+ @media (min-width: 992px) {
+	 #gallery img {
+		 height: 18vw;
+	}
+}
+#imgModal .carousel-item img {
+	 height: 60vw;
+	 object-fit: cover;
+}
+ @media (min-width: 576px) {
+	 #imgModal .carousel-item img {
+		 height: 350px;
+	}
+}
+		</style>
 	</head>
 
 	<body>
@@ -30,6 +55,7 @@
 				</div>
 				<hr>
 				<div class="row">
+					<!--
 					<div class="col-md-4">
 						<div class="list-group">
 							<a href="#Photos" class="list-group-item list-group-item-action">Photo Gallery</a>
@@ -37,70 +63,149 @@
 							<a href="#Posters" class="list-group-item list-group-item-action">Movie Posters</a>
 						</div>
 					</div>
-					<div class="col-md-8">
+					-->
+					<div class="col">
 						<h2 id="Photos">Photo Gallery</h2>
-						<div class="row text-center text-lg-left">
+						<div class="row no-gutters text-center" id="gallery" data-toggle="modal" data-target="#imgModal">
 							<div class="col-lg-3 col-md-4 col-6">
-								<a href="#" class="d-block mb-4 h-100">
-									<img class="img-fluid img-thumbnail" src="https://source.unsplash.com/pWkk7iiCoDM/400x300" alt="">
+								<a href="#" class="d-block h-100" data-target="#imgCarousel" data-slide-to="0">
+									<img class="img-fluid" src="../img/gallery/11069496_1464900117146557_1578034340060294438_o.jpg" alt="">
 								</a>
 							</div>
 							<div class="col-lg-3 col-md-4 col-6">
-								<a href="#" class="d-block mb-4 h-100">
-									<img class="img-fluid img-thumbnail" src="https://source.unsplash.com/aob0ukAYfuI/400x300" alt="">
+								<a href="#" class="d-block h-100" data-target="#imgCarousel" data-slide-to="1">
+									<img class="img-fluid" src="../img/gallery/11145567_1467599236876645_7030072806701959681_o.jpg" alt="">
 								</a>
 							</div>
 							<div class="col-lg-3 col-md-4 col-6">
-								<a href="#" class="d-block mb-4 h-100">
-									<img class="img-fluid img-thumbnail" src="https://source.unsplash.com/EUfxH-pze7s/400x300" alt="">
+								<a href="#" class="d-block h-100" data-target="#imgCarousel" data-slide-to="2">
+									<img class="img-fluid" src="../img/gallery/12087277_1493007704335798_1645579304536348900_o.jpg" alt="">
 								</a>
 							</div>
 							<div class="col-lg-3 col-md-4 col-6">
-								<a href="#" class="d-block mb-4 h-100">
-									<img class="img-fluid img-thumbnail" src="https://source.unsplash.com/M185_qYH8vg/400x300" alt="">
+								<a href="#" class="d-block h-100" data-target="#imgCarousel" data-slide-to="3">
+									<img class="img-fluid" src="../img/gallery/12640515_10208173978244314_2279281443260879185_o.jpg" alt="">
 								</a>
 							</div>
 							<div class="col-lg-3 col-md-4 col-6">
-								<a href="#" class="d-block mb-4 h-100">
-									<img class="img-fluid img-thumbnail" src="https://source.unsplash.com/sesveuG_rNo/400x300" alt="">
+								<a href="#" class="d-block h-100" data-target="#imgCarousel" data-slide-to="4">
+									<img class="img-fluid" src="../img/gallery/13071995_1547620732207828_6591589032638920204_o.jpg" alt="">
 								</a>
 							</div>
 							<div class="col-lg-3 col-md-4 col-6">
-								<a href="#" class="d-block mb-4 h-100">
-									<img class="img-fluid img-thumbnail" src="https://source.unsplash.com/AvhMzHwiE_0/400x300" alt="">
+								<a href="#" class="d-block h-100" data-target="#imgCarousel" data-slide-to="5">
+									<img class="img-fluid" src="../img/gallery/20180630_161014.jpg" alt="">
 								</a>
 							</div>
 							<div class="col-lg-3 col-md-4 col-6">
-								<a href="#" class="d-block mb-4 h-100">
-									<img class="img-fluid img-thumbnail" src="https://source.unsplash.com/2gYsZUmockw/400x300" alt="">
+								<a href="#" class="d-block h-100" data-target="#imgCarousel" data-slide-to="6">
+									<img class="img-fluid" src="../img/gallery/20180630_162044.jpg" alt="">
 								</a>
 							</div>
 							<div class="col-lg-3 col-md-4 col-6">
-								<a href="#" class="d-block mb-4 h-100">
-									<img class="img-fluid img-thumbnail" src="https://source.unsplash.com/EMSDtjVHdQ8/400x300" alt="">
+								<a href="#" class="d-block h-100" data-target="#imgCarousel" data-slide-to="7">
+									<img class="img-fluid" src="../img/gallery/33580923_1830388327264399_5557290211865526272_o.jpg" alt="">
 								</a>
 							</div>
 							<div class="col-lg-3 col-md-4 col-6">
-								<a href="#" class="d-block mb-4 h-100">
-									<img class="img-fluid img-thumbnail" src="https://source.unsplash.com/8mUEy0ABdNE/400x300" alt="">
+								<a href="#" class="d-block h-100" data-target="#imgCarousel" data-slide-to="8">
+									<img class="img-fluid" src="../img/gallery/33597404_10215811655661476_150419143504429056_o.jpg" alt="">
 								</a>
 							</div>
 							<div class="col-lg-3 col-md-4 col-6">
-								<a href="#" class="d-block mb-4 h-100">
-									<img class="img-fluid img-thumbnail" src="https://source.unsplash.com/G9Rfc1qccH4/400x300" alt="">
+								<a href="#" class="d-block h-100" data-target="#imgCarousel" data-slide-to="9">
+									<img class="img-fluid" src="../img/gallery/33751432_10215815359234063_5258817545598664704_o.jpg" alt="">
 								</a>
 							</div>
 							<div class="col-lg-3 col-md-4 col-6">
-								<a href="#" class="d-block mb-4 h-100">
-									<img class="img-fluid img-thumbnail" src="https://source.unsplash.com/aJeH0KcFkuc/400x300" alt="">
+								<a href="#" class="d-block h-100" data-target="#imgCarousel" data-slide-to="10">
+									<img class="img-fluid" src="../img/gallery/33777441_10215827182609640_1183801142644047872_o.jpg" alt="">
 								</a>
 							</div>
 							<div class="col-lg-3 col-md-4 col-6">
-								<a href="#" class="d-block mb-4 h-100">
-									<img class="img-fluid img-thumbnail" src="https://source.unsplash.com/p2TQ-3Bh3Oo/400x300" alt="">
+								<a href="#" class="d-block h-100" data-target="#imgCarousel" data-slide-to="11">
+									<img class="img-fluid" src="../img/gallery/40528727_1909054116064486_1236367805725540352_o.jpg" alt="">
 								</a>
 							</div>
-						</div><!-- /row -->
+						</div><!-- /gallery row -->
+						<div class="modal fade" id="imgModal" tabindex="-1" role="dialog" aria-hidden="true">
+							<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+								<div class="modal-content">
+									<div class="modal-header">
+										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+											<span aria-hidden="true">&times;</span>
+										</button>
+									</div>
+									<div class="modal-body">
+										<div id="imgCarousel" class="carousel slide" data-ride="carousel">
+											<ol class="carousel-indicators">
+												<li data-target="#imgCarousel" data-slide-to="0" class="active"></li>
+												<li data-target="#imgCarousel" data-slide-to="1"></li>
+												<li data-target="#imgCarousel" data-slide-to="2"></li>
+												<li data-target="#imgCarousel" data-slide-to="3"></li>
+												<li data-target="#imgCarousel" data-slide-to="4"></li>
+												<li data-target="#imgCarousel" data-slide-to="5"></li>
+												<li data-target="#imgCarousel" data-slide-to="6"></li>
+												<li data-target="#imgCarousel" data-slide-to="7"></li>
+												<li data-target="#imgCarousel" data-slide-to="8"></li>
+												<li data-target="#imgCarousel" data-slide-to="9"></li>
+												<li data-target="#imgCarousel" data-slide-to="10"></li>
+												<li data-target="#imgCarousel" data-slide-to="11"></li>
+											</ol>
+											<div class="carousel-inner">
+												<div class="carousel-item active">
+													<img class="d-block w-100" src="../img/gallery/11069496_1464900117146557_1578034340060294438_o.jpg">
+												</div>
+												<div class="carousel-item">
+													<img class="d-block w-100" src="../img/gallery/11145567_1467599236876645_7030072806701959681_o.jpg">
+												</div>
+												<div class="carousel-item">
+													<img class="d-block w-100" src="../img/gallery/12087277_1493007704335798_1645579304536348900_o.jpg">
+												</div>
+												<div class="carousel-item">
+													<img class="d-block w-100" src="../img/gallery/12640515_10208173978244314_2279281443260879185_o.jpg">
+												</div>
+												<div class="carousel-item">
+													<img class="d-block w-100" src="../img/gallery/13071995_1547620732207828_6591589032638920204_o.jpg">
+												</div>
+												<div class="carousel-item">
+													<img class="d-block w-100" src="../img/gallery/20180630_161014.jpg">
+												</div>
+												<div class="carousel-item">
+													<img class="d-block w-100" src="../img/gallery/20180630_162044.jpg">
+												</div>
+												<div class="carousel-item">
+													<img class="d-block w-100" src="../img/gallery/33580923_1830388327264399_5557290211865526272_o.jpg">
+												</div>
+												<div class="carousel-item">
+													<img class="d-block w-100" src="../img/gallery/33597404_10215811655661476_150419143504429056_o.jpg">
+												</div>
+												<div class="carousel-item">
+													<img class="d-block w-100" src="../img/gallery/33751432_10215815359234063_5258817545598664704_o.jpg">
+												</div>
+												<div class="carousel-item">
+													<img class="d-block w-100" src="../img/gallery/33777441_10215827182609640_1183801142644047872_o.jpg">
+												</div>
+												<div class="carousel-item">
+													<img class="d-block w-100" src="../img/gallery/40528727_1909054116064486_1236367805725540352_o.jpg">
+												</div>
+											</div>
+											<a class="carousel-control-prev" href="#imgCarousel" role="button" data-slide="prev">
+												<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+												<span class="sr-only">Previous</span>
+											</a>
+											<a class="carousel-control-next" href="#imgCarousel" role="button" data-slide="next">
+												<span class="carousel-control-next-icon" aria-hidden="true"></span>
+												<span class="sr-only">Next</span>
+											</a>
+										</div>
+									</div>
+									<div class="modal-footer">
+										<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+									</div>
+								</div>
+							</div>
+						</div><!-- /imgModal -->
 						<hr>
 						<h2 id="Videos">Videos</h2>
 						<div class="row">
