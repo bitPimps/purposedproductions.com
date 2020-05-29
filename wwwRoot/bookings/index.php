371ad90eb75b001b31c2784f373dd83ab5855d11
bitPimps <bitpimps@gmail.com>
2020-05-29 09:07:34 -0500

    Added Bookings
    Added Bookings section per Jana

---
+++ wwwRoot/bookings/index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Bookings</title>
+		<meta name="description" content="">
+		<?php include_once("../inc/meta-data.php"); ?>
+	</head>
+
+	<body>
+		<?php include_once("../inc/nav-main.php"); DrawNavMain("Bookings"); ?>
+		<nav id="breadcrumb" aria-label="breadcrumb">
+			<div class="container">
+				<ol class="breadcrumb">
+					<li class="breadcrumb-item"><a href="/index.php">Home</a></li>
+					<li class="breadcrumb-item active" aria-current="page">Bookings</li>
+				</ol>
+			</div>
+		</nav>
+		<main role="main">
+			<section class="container py-5">
+				<div class="row">
+					<div class="col-md-8">
+						<h1>Bookings</h1>
+						<p class="lead">Representing speakers and artists alike.</p>
+					</div>
+					<div class="col-md-4">
+					</div>
+				</div>
+				<hr>
+				<div class="row">
+					<div class="col-md-8">
+						<h2>Book Now!</h2>
+						<p class="lead">Owner and CEO of It's A Gamble LLC DBA Purposed Productions</p>
+						<p>Click images to enlarge.</p>
+					</div>
+					<div class="col-md-4">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<a href="../img/Speakers-Artists-Topics.jpg" target="_blank">
+							<img class="img-thumbnail" src="../img/Speakers-Artists-Topics.jpg">
+						</a>
+					</div>
+					<div class="col-md-6">
+						<a href="../img/Speakers-Artists.jpg" target="_blank">
+							<img class="img-thumbnail" src="../img/Speakers-Artists.jpg">
+						</a>
+					</div>
+				</div>
+			</section>
+		</main>
+		<?php include_once("../inc/footer.php"); ?>
+	</body>
+</html>
