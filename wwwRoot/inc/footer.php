56ae6ac2f4709fb99e357224adbf06148efdbab7
bitPimps <bitpimps@gmail.com>
2020-09-01 14:14:26 -0500

    Business Card and Bootstrap upgrade

--- wwwRoot/inc/footer.php
+++ wwwRoot/inc/footer.php
@@ -15,8 +15,8 @@
 			</div>
 		</footer>
 		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>
 		<script>
 			WebFontConfig = {
