7e51395463d8ab896619f4fbbc9a886d1828a0b6
bitPimps <bitpimps@gmail.com>
2020-10-08 10:24:44 -0500

    Covid and BLM statements, Philanthropy and logos
    Updates requested from Jana

--- wwwRoot/about/index.php
+++ wwwRoot/about/index.php
@@ -34,10 +34,10 @@
 						<h2>Jana M. Gamble</h2>
 						<p class="lead">Owner and CEO of It's A Gamble LLC DBA Purposed Productions</p>
 						<p>Jana M. Gamble, is a Producer, Screenwriter, Director, Assistant Director, Speaker,
-Author & Advocate. She was born in the small town of Moberly, Missouri to two loving
+Author &amp; Advocate. She was born in the small town of Moberly, Missouri to two loving
 parents- Ron Gamble, an African American man who was a MO State Trooper and
 Sandy Gamble a Mexican American woman who worked in childcare. Jana was born
-with the desire and tenacity to be a great leader. Following her parents’ divorce Gamble
+with the desire and tenacity to be a great leader. Following her parents' divorce Gamble
 was raised by her single mother who was a foster mother to many and volunteered
 religiously in the community. Jana stands solid on this foundation of giving and lives by
 a philanthropic lifestyle always helping those in need. Gamble sits on several non-profit
@@ -47,7 +47,7 @@ Gamble went from hosting her own shows to an imaginary audience as a child to
 fearlessly leading her senior high school Broadcasting program both her junior and
 senior years. Gamble became a self-taught anchor, script writer and video editor for her
 school. With a passion for film, Jana moved to St. Louis, MO (StL) in 2000 to attend
-Webster University for film production. After completing her freshman year Gamble’s life
+Webster University for film production. After completing her freshman year Gamble's life
 would lead her on other routes for the next 12 years, but they always involved producing
 events and various forms of media for both not-for-profit and for profit businesses.
 During that time Gamble became a speaker, self-published author publishing 3 books,
@@ -59,22 +59,22 @@ wearing multiple hats and learning everything she could. She worked as a Produce
 Assistant Director and Screenwriter creating independent films and television show
 pilots in a wide range of genres. Finally her life had come full circle and she began living
 her dream of being a filmmaker. One year later, Jana decided to start her own
-production company called It’s A Gamble Production. It's A Gamble Production provides
+production company called It's A Gamble Production. It's A Gamble Production provides
 independent filmmakers assistance during development, pre-production and production
 with film and television projects. Services vary from Consulting, Administrative duties,
 Screenwriting, Casting, Location Scouting, Assistant Director responsibilities, Craft
 Services, providing Production Assistants and more. It's A Gamble Production also
 assists non-profits with behind the scenes photography and video production on a
 limited basis.</p>
-<p>Due to her involvement as the VP and managing the PR for Mike Brown, Sr.’s Chosen
+<p>Due to her involvement as the VP and managing the PR for Mike Brown, Sr.'s Chosen
 for Change Foundation during the unrest in Ferguson, MO in 2014, Gamble and her
 company became targets for unwarranted death threats and negativity. Months later, as
 the StL/Ferguson community began to return to a new normal, Gamble decided to
 change the name of her company to Purposed Productions. Gamble continues to
 partner with Lovelace Films from StL to LA to produce original, independent film and tv
-such as The First Pick, Eyes Don’t Lie and Rock the movie. Jana is a Member and
+such as The First Pick, Eyes Don't Lie and Rock the movie. Jana is a Member and
 Moderator for the Organization of Black Screenwriters. From the faith-based,
-book-to-film and even straight hood scripts- Gamble’s voice is being heard. Purposed
+book-to-film and even straight hood scripts- Gamble's voice is being heard. Purposed
 Productions is bringing Hollywood to St. Louis through film, tv and providing youth
 opportunities to explore history & the healing art of filmmaking. Gamble resides in St.
 Louis, MO with her two amazing teenage children.</p>
@@ -115,7 +115,7 @@ Louis, MO with her two amazing teenage children.</p>
 				</div>
 				<hr>
 				<h2>Purchase Books by Jana M. Gamble!</h2>
-				<div class="row">
+				<div class="row mb-5">
 					<div class="col-lg-3 col-md-6">
 						<div class="card">
 							<img class="card-img-top" src="../img/107-Ways-to-Give.jpg">
@@ -163,6 +163,34 @@ Louis, MO with her two amazing teenage children.</p>
 								</a>
 							</div>
 						</div><!-- /card -->
+					</div>
+				</div>
+				<hr>
+				<h3>COVID Statement</h3>
+				<div class="row mb-5">
+					<div class="col-md-8">
+						<p>Due to the COVID-19 pandemic, Purposed Productions is limiting productions at this time. NEW Virtual Production Services, Consulting Services, Screenwriting, and Speaker & Artist performances are actively available. We are happy to help you from a distance and look forward to resuming full productions after this traumatic season concludes. Contact us directly for more information.</p>
+						<p>Practice social distancing, wear your personal protective equipment, wash your hands, and stay blessed- On Purpose!</p>
+					</div>
+					<div class="col-md-4">
+						<img class="img-fluid" src="../img/logos/VLAA.jpg" alt="Volunteer Lawyers & Accountants">
+					</div>
+				</div>
+				<hr>
+				<h3>Our Stand for Black Lives Matter &amp; of Racial Healing in our country</h3>
+				<div class="row mb-5">
+					<div class="col-lg-6">
+						<img class="img-fluid" src="../img/pages/BLM-Statement.png">
+					</div>
+					<div class="col-lg-6 pt-5">
+						<img class="img-fluid mt-5" src="../img/pages/mlk-quote-4-justice.jpg">
+					</div>
+				</div>
+				<hr>
+				<h3>Philanthropy / Organizations we Sponsor</h3>
+				<div class="row">
+					<div class="col-md-6">
+						<img class="img-fluid" src="../img/logos/Chosen-For-Change.png" alt="Chosen For Change">
 					</div>
 				</div>
 			</section>
