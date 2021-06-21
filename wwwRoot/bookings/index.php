3a5fcfc3db4b6b86a243267bbee50ec73fb205ce
bitPimps <bitpimps@gmail.com>
2021-06-21 11:00:45 -0500

    Update Upcoming Events

--- wwwRoot/bookings/index.php
+++ wwwRoot/bookings/index.php
@@ -31,7 +31,7 @@
 				<hr>
 				<section id="events">
 				<div class="row">
-					<div class="col-md-8">
+					<div class="col">
 						<h2>Upcoming Events</h2>
 						<table class="table">
 							<thead class="thead-light">
@@ -42,24 +42,113 @@
 							<tbody>
 								<tr>
 									<td>
-										<a href="https://zoom.us/meeting/register/tJMlc--orzMiE9AKlLKF8ZpN2bQwQeNshdOi" target="_blank">
-											Writing On Purpose | Screenwriters' Accountability Lab
-										</a>
+										<div class="row">
+											<div class="col-lg-4">
+												<p><strong>1st Tuesday of The Month</strong><br>
+													<em>Writing On Purpose</em></p>
+											</div>
+											<div class="col-lg-4">
+												<p>1:00 - 2:30pm CST<br>
+													Accountability Lab + Q&A</p>
+											</div>
+											<div class="col-lg-4">
+												<p>Where: @RISE & via Zoom<br>
+													8820 Ladue Rd., StL, 63124<br>
+													Register at <a href="mailto:PurposedProductions@gmail.com">PurposedProductions@gmail.com</a></p>
+											</div>
+										</div>
 									</td>
 								</tr>
 								<tr>
 									<td>
-										<a href="https://zoom.us/meeting/register/tJ0tfuqrpzwjGtzrajI4bACU8oHxTYzLAGJO" target="_blank">
-											You are invited to HPNStL's Zoom meeting<br>
-											Jun 9, 2021 10:30 AM Central Time (US and Canada)
-										</a><br>
-										<small class="text-muted">After registering, you will receive a confirmation email containing information about joining the meeting.</small>
+										<div class="row">
+											<div class="col-lg-4">
+												<p><strong>2nd Wednesday of The Month</strong><br>
+													<em>Warrior Wednesdays</em></p>
+											</div>
+											<div class="col-lg-4">
+												<p>12:00 - 1:00pm CST</p>
+											</div>
+											<div class="col-lg-4">
+												<p>Join us spread the L.O.V.E, pray for people, the projects & the issues the plague both one city & Hollywood- On Purpose!</p>
+											</div>
+										</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="row">
+											<div class="col-lg-4">
+												<p><strong>Every Friday</strong><br>
+													<em>Prayer Call</em></p>
+											</div>
+											<div class="col-lg-4">
+												<p>2:00 - 3:00pm CST</p>
+											</div>
+											<div class="col-lg-4">
+												<p>Join our faithful HPN members from all over the world to pray for the entertainment industry every Friday! For security, please email <a href="mailto:kelry@hpnemail.org">kelry@hpnemail.org</a> for the Zoom link.</p>
+											</div>
+										</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="row">
+											<div class="col-lg-4">
+												<p><strong>Against All Odds<br>
+													Jana M. Gamble</strong><br>
+													<em>Beating Cancer</em></p>
+											</div>
+											<div class="col-lg-4">
+												<p>9:00 - 11:00am CST<br>
+													Wednesday, June 23, 2021</p>
+											</div>
+											<div class="col-lg-4">
+												<p>Where: via Zoom</p>
+												<p><a href="https://www.facebook.com/connectingandpromotingwomen">Visit Connecting And Promoting Women on Facebook to register!</a></p>
+											</div>
+										</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="row">
+											<div class="col-lg-4">
+												<p><strong>Power Up: Fireside Chat with Jana M. Gamble</strong><br>
+													<em>Creativity & Innovation</em></p>
+											</div>
+											<div class="col-lg-4">
+												<p>9:00 - 10:00am CST<br>
+													Wednesday, July 14, 2021</p>
+											</div>
+											<div class="col-lg-4">
+												<p>Where: @RISE & via Zoom<br>
+													8820 Ladue Rd., StL, 63124</p>
+												<p><a href="https://app.hatchbuck.com/OnlineForm/10241400260">Register today!</a></p>
+											</div>
+										</div>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<div class="row">
+											<div class="col-lg-4">
+												<p><strong>Hollywood Prayer Network 20th Birthday</strong></p>
+											</div>
+											<div class="col-lg-4">
+												<p>5:00 - 9:00pm CST<br>
+													Saturday, August 14, 2021</p>
+											</div>
+											<div class="col-lg-4">
+												<p>First Presbyterian Church of Hollywood<br>
+													1760 North Gower Street, Los Angeles, CA 90028</p>
+												<p>General Admission: Free | VIP: $25</p>
+											</div>
+										</div>
 									</td>
 								</tr>
 							</tbody>
 						</table>
-					</div>
-					<div class="col-md-4">
 					</div>
 				</div>
 				</section>
